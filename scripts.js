c96b1d0fc193427743e8c9b61585b4e8401443f8
Gabriel Arruda <gabriel.arruda@oktagon.com.br>
2022-06-22 23:58:41 -0300

    new: keyboard interactions with the guessed words

--- scripts.js
+++ scripts.js
@@ -14,6 +14,10 @@ function changeRanking2() {
 
 function toggleDarkMode() {
     document.body.classList.toggle("dark");
+    // const squares = document.querySelectorAll(".square");
+    // squares.forEach((element) => {
+    //     element.classList.toggle("dark-square");
+    // });
 }
 
 const word = "PILHA";
@@ -23,6 +27,7 @@ const YCount = 5;
 
 const QWERTY = "QWERTYUIOPASDFGHJKLZXCVBNM";
 let squares = [];
+let buttons = [];
 
 let guesses = [];
 let guess = [];
@@ -30,7 +35,7 @@ let cursorPosition = 0;
 let guessCount = 0;
 
 function init() {
-    const buttons = document.querySelectorAll(".keyboardKey");
+    buttons = document.querySelectorAll(".keyboardKey");
     buttons.forEach((element, i) => {
         element.onclick = (e) => {
             handleInput(i);
@@ -78,22 +83,67 @@ function updateGuess() {
     }
 }
 
+function updateKeyboard() {
+    buttons.forEach((element, index) => {
+        let next = false;
+        for (let i = 0; i < guesses.length && !next; i++) {
+            for (let j = 0; j < guesses[i].length && !next; j++) {
+                if (guesses[i][j].letter === QWERTY[index]) {
+                    if (guesses[i][j].color === LetterType.RED) {
+                        element.style.backgroundColor = LetterType.GREY;
+                        element.children[0].style.color = "#757575";
+                        element.style.cursor = "default";
+                    } else {
+                        element.style.backgroundColor = guesses[i][j].color;
+                    }
+
+                    if (guesses[i][j].color === LetterType.GREEN) {
+                        next = true;
+                        break;
+                    }
+
+                    if (guesses[i][j].color === LetterType.YELLOW) {
+                        next = true;
+                        break;
+                    }
+                }
+            }
+        }
+    });
+}
+
 function checkGuess() {
     let guessed = true;
+    let lineGuess = [];
     for (let i = 0; i < XCount; i++) {
         if (guess[i] === word[i]) {
-            squares[guessCount][i].style.backgroundColor = "#88F783";
+            squares[guessCount][i].style.backgroundColor = LetterType.GREEN;
+            lineGuess.push({
+                letter: guess[i],
+                color: LetterType.GREEN,
+            });
             continue;
         }
 
         if (word.includes(guess[i])) {
-            squares[guessCount][i].style.backgroundColor = "#F2F559";
+            squares[guessCount][i].style.backgroundColor = LetterType.YELLOW;
+            lineGuess.push({
+                letter: guess[i],
+                color: LetterType.YELLOW,
+            });
         } else {
-            squares[guessCount][i].style.backgroundColor = "#F5A759";
+            squares[guessCount][i].style.backgroundColor = LetterType.RED;
+            lineGuess.push({
+                letter: guess[i],
+                color: LetterType.RED,
+            });
         }
 
         guessed = false;
     }
+
+    guesses.push(lineGuess);
+    updateKeyboard();
 
     if (!guessed) {
         guessCount++;
@@ -107,3 +157,10 @@ function checkGuess() {
         alert("WIN");
     }
 }
+
+const LetterType = {
+    GREEN: "#88F783",
+    YELLOW: "#F2F559",
+    RED: "#F5A759",
+    GREY: "#CACACA",
+};
